9229d18e393a6b3ece1f821f661453a2bd366bcd
Daniel Tomlinson <Daniel@Daniels-MacBook-Air.local>
2014-09-02 17:19:31 +0100

    Initial Commit

---
+++ DANGoogleSignIn/DANGoogleSignIn.h
@@ -0,0 +1,13 @@
+//
+//  DANGoogleSignIn.h
+//  DANGoogleSignIn
+//
+//  Created by Daniel Tomlinson on 02/09/2014.
+//  Copyright (c) 2014 Rocket Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DANGoogleSignIn : NSObject
+
+@end
---
+++ DANGoogleSignIn/DANGoogleSignIn.m
@@ -0,0 +1,13 @@
+//
+//  DANGoogleSignIn.m
+//  DANGoogleSignIn
+//
+//  Created by Daniel Tomlinson on 02/09/2014.
+//  Copyright (c) 2014 Rocket Apps. All rights reserved.
+//
+
+#import "DANGoogleSignIn.h"
+
+@implementation DANGoogleSignIn
+
+@end
