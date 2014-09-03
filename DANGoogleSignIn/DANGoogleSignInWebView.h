dcdc052c46b1e9b70d2c7f30844f2b1e69c18f6a
Danielle Lancashire <dani@builds.terrible.systems>
2014-09-03 14:18:45 +0100

    Setup

--- DANGoogleSignIn/DANGoogleSignInWebView.h
+++ DANGoogleSignIn/DANGoogleSignInWebView.h
@@ -1,10 +1,13 @@
 //
-//  DANGoogleSignIn.h
+//  DANGoogleSignInWebView.h
 //  DANGoogleSignIn
 //
 //  Created by  Danielle Lancashireon 02/09/2014.
 //  Copyright (c) 2014 Rocket Apps. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
+@interface DANGoogleSignInWebView : UIWebView
+
+@end
