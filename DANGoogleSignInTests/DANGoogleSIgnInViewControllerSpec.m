3fb645f146593d32ba2019f97885fcf71f712228
Danielle Lancashire <dani@builds.terrible.systems>
2014-09-08 13:40:48 +0100

    Add Unit tests

---
+++ DANGoogleSignInTests/DANGoogleSIgnInViewControllerSpec.m
@@ -0,0 +1,37 @@
+//
+//  DANGoogleSIgnInViewControllerSpec.m
+//  DANGoogleSignIn
+//
+//  Created by  Danielle Lancashireon 08/09/2014.
+//  Copyright 2014 Rocket Apps. All rights reserved.
+//
+
+#import <Specta/Specta.h>
+#define EXP_SHORTHAND
+#import <Expecta/Expecta.h>
+
+#import "DANGoogleSIgnInViewController.h"
+
+SpecBegin(DANGoogleSIgnInViewController)
+
+describe(@"DANGoogleSIgnInViewController", ^{
+  __block DANGoogleSignInViewController *viewController;
+
+  context(@"when initilized using the designated initiliser", ^{
+    beforeEach(^{
+      viewController = [[DANGoogleSignInViewController alloc] initWithClientId:@"hello"
+                                                                  clientSecret:@"world"
+                                                                        scopes:@[]];
+    });
+
+    it(@"should not be nil", ^{
+      expect(viewController).toNot.beNil();
+    });
+
+    afterEach(^{
+      viewController = nil;
+    });
+  });
+});
+
+SpecEnd
